d77c963c6cb024e4da671a2028334f8f78552d8f
허준석 <heojunseog@heojunseog-ui-MacBookPro.local>
2025-07-04 00:59:25 +0700

    feat: Add Vite React project with Tailwind CSS and dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3ECF8E"
+      }
+    },
+  },
+  plugins: [],
+}
+
